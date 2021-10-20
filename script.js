4b4ea61af6cbed1e72db3fbf2a64472869047514
trimagnus <tri_magnus@hotmail.com>
2021-10-20 12:52:22 -0700

    Refactored code and finished core functionality. Needs error checking and an UI to be complete.

--- script.js
+++ script.js
@@ -1,46 +1,75 @@
 //Rock, Paper, Scissors game script
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === computerSelection) {
-        return `You both chose ${playerSelection}. This match is a tie!`;
-    }
-    if((playerSelection === 'rock' && computerSelection === 'paper') ||
-       (playerSelection === 'scissors' && computerSelection === 'rock') ||
-       (playerSelection === 'paper' && computerSelection === 'scissors')){
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
-    } else {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
-    }
+
+let matchStrings = {
+    'tie': 'ties with',
+    'lose': 'loses to',
+    'win': 'beats'
 }
 
-function game() {
-    const maxRounds = 3;
-    let currentRound = 0;
-    let wins = 0;
-    while(currentRound < maxRounds){
-        currentRound++;
-        console.log('Lets play a round of Rock, Paper, Scissors');
-        console.log(`Current round: ${currentRound}. Wins: ${wins}`)
-        let playerInput = prompt('Chose: rock, paper, or scissors');
-        let matchResult = playRound(playerInput, getComputerGuess());
-        if(matchResult.split(' ')[1] === 'win!') {
-            wins++;
-        }
-        console.log(matchResult);
-    }
+function getPlayerChoice() {
+    let choice = prompt('Chose: rock, paper, or scissors')
+    choice = choice[0].toUpperCase() + choice.substr(1).toLowerCase();
+    return choice;
 }
 
-function getComputerGuess(){
-    let _guesses = ['rock', 'paper', 'scissors'];
-    let _guessNum = randRange(3);
-    let _guess = _guesses[_guessNum];
-    console.log(_guess);
-    return _guess
+function getComputerChoice(){
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let choiceNum = randRange(3);
+    let choice = choices[choiceNum];
+    console.log('Computer choice: ' + choice);
+    return choice;
 }
 
 //Return a number from 1 to stop, inclusive
 function randRange(stop) {
-    return Math.floor(Math.random() * stop) + 1;
+    return Math.floor(Math.random() * stop);
+}
+
+function getMatchResult(pchoice, cchoice) {
+    if (pchoice === cchoice) {
+        return 'tie';
+    } else if ((pchoice === 'Rock' && cchoice === 'Paper') ||
+               (pchoice === 'Scissors' && cchoice === 'Rock') ||
+               (pchoice === 'Paper' && cchoice === 'Scissors')) {
+        return 'lose';
+    } else {
+        return 'win';
+    }
+}
+
+//One round of the game. Returns true if player scores a point
+function playRound() {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+
+    let matchResult = getMatchResult(playerChoice, computerChoice);
+
+    console.log(`${playerChoice} ${matchStrings[matchResult]} ${computerChoice}.`)
+    return matchResult === 'win'? true : false;
+}
+
+function game() {
+    //Display Intro
+    console.log('Lets play a game of Rock, Paper, Scissors!');
+
+    const maxRounds = 3;
+    let currentRound = 0;
+    let score = 0;
+
+    while(currentRound < maxRounds){
+        currentRound++;
+        console.log(`Current round: ${currentRound}. Score: ${score}`);
+
+        if (playRound()) {
+            score++;
+        }
+    }
+    console.log('Final score: ' + score);
+    if (score > 1) {
+        console.log('You win!')
+    } else {
+        console.log('You lost. Better luck next time!')
+    }
 }
 
 game();
