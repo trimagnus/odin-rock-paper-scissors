efa16d027527dbbc876be92955bbdd9c148a5c8a
trimagnus <tri_magnus@hotmail.com>
2021-10-25 12:33:08 -0700

    Updated script to reflect update to gui, modified win condition

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 //Rock, Paper, Scissors game script
 
-let matchStrings = {
-    'tie': 'ties with',
-    'lose': 'loses to',
-    'win': 'beats'
-}
-
-function getPlayerChoice() {
-    let choice = prompt('Chose: rock, paper, or scissors')
-    choice = choice[0].toUpperCase() + choice.substr(1).toLowerCase();
-    return choice;
-}
-
 function getComputerChoice(){
-    let choices = ['Rock', 'Paper', 'Scissors'];
+    let choices = ['rock', 'raper', 'scissors'];
     let choiceNum = randRange(3);
     let choice = choices[choiceNum];
-    console.log('Computer choice: ' + choice);
+    //console.log('Computer choice: ' + choice);
     return choice;
 }
 
@@ -28,48 +16,56 @@ function randRange(stop) {
 function getMatchResult(pchoice, cchoice) {
     if (pchoice === cchoice) {
         return 'tie';
-    } else if ((pchoice === 'Rock' && cchoice === 'Paper') ||
-               (pchoice === 'Scissors' && cchoice === 'Rock') ||
-               (pchoice === 'Paper' && cchoice === 'Scissors')) {
+    } else if ((pchoice === 'rock' && cchoice === 'paper') ||
+               (pchoice === 'scissors' && cchoice === 'rock') ||
+               (pchoice === 'paper' && cchoice === 'scissors')) {
         return 'lose';
     } else {
         return 'win';
     }
 }
 
-//One round of the game. Returns true if player scores a point
-function playRound() {
-    let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
+const playerScoreText = document.getElementById('player-score');
+const playerScoreTextParent = playerScoreText.parentNode;
+const cpuScoreText = document.getElementById('cpu-score');
+const cpuScoreTextParent = cpuScoreText.parentNode;
 
-    let matchResult = getMatchResult(playerChoice, computerChoice);
+let pScore = 0;
+let cScore = 0;
 
-    console.log(`${playerChoice} ${matchStrings[matchResult]} ${computerChoice}.`)
-    return matchResult === 'win'? true : false;
-}
+const updateScoreBoard = () => {
+    playerScoreText.textContent = pScore;
+    cpuScoreText.textContent = cScore;
+};
 
-function game() {
-    //Display Intro
-    console.log('Lets play a game of Rock, Paper, Scissors!');
-
-    const maxRounds = 3;
-    let currentRound = 0;
-    let score = 0;
-
-    while(currentRound < maxRounds){
-        currentRound++;
-        console.log(`Current round: ${currentRound}. Score: ${score}`);
-
-        if (playRound()) {
-            score++;
-        }
+const playRound = (e) => {
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    const matchResult = getMatchResult(playerChoice, computerChoice);
+    if(matchResult === 'win') {
+        pScore++;
+    } else if (matchResult === 'lose') {
+        cScore++;
     }
-    console.log('Final score: ' + score);
-    if (score > 1) {
-        console.log('You win!')
-    } else {
-        console.log('You lost. Better luck next time!')
+    updateScoreBoard();
+    let endGame = false;
+    if(pScore === 5){
+        playerScoreTextParent.classList.add('winner');
+        cpuScoreTextParent.classList.add('loser');
+        endGame = true;
+    } else if(cScore === 5) {
+        playerScoreTextParent.classList.add('loser');
+        cpuScoreTextParent.classList.add('winner');
+        endGame = true;
     }
-}
+    if(endGame) {
+        buttons.forEach((button) => {
+            button.removeEventListener('click', playRound);
+        });
+    }
+};
 
-//game();
+const buttons = document.querySelectorAll('.rps-button');
+buttons.forEach((button) => {
+    button.addEventListener('click', playRound);
+});
